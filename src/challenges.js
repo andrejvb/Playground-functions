0cc0c7a5d3576cccf4da49f6a660fc8fb9f5ae92
andrejvb <andre.jvb@gmail.com>
2022-09-27 19:39:19 -0300

    Resolvido Desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -24,27 +24,59 @@ function splitSentence(param1) {
 
 // Desafio 4
 
+// function concatName(param1, from, to) {
+//   param1.splice(to, 0, param1.splice(from, param1.length - 1)[0]);
+//   return  param1
+// }
+
 function concatName(param1) {
   return param1[param1.length -1] + ', ' + param1[0]
 }
 
-
-let nomes = ["Andre", "Rapha", "caique", "Ze"];
-concatName(nomes)
-
-console.log(concatName(nomes));
+// let nomes = ["Andre", "Rapha", "Ze"];
+// concatName(nomes)
+// console.log(nomes);
 
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+
+
+
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
 }
+
+
+// console.log(footballPoints(20, 20));
+
+
+
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
 
+// function highestCount(param1) {
+//   for (let index = 0; index < param1.length; index += 1) {
+//         if (param1 > param1[index]) {
+//       numeroMaior = param1[index]
+//     };
+   
+//   }
+// }
+
+// console.log(highestCount("[9, 1, 2, 3, 9, 5, 7]"));
+
+function highestCount(param1) {
+  let numeroMaior = param1[0]
+  for (let index = 0; index < param1.length; index += 1) {
+        if (param1[index] > numeroMaior) {
+      numeroMaior = param1[index]
+    }; return numeroMaior
+      
+  } 
+  console.log(highestCount("[9, 1, 2, 3, 9, 5, 7]"));
+
+  
+}
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
