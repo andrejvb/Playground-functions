3c6f1c805a6e68b8ae0262d08d7a400c45ae40d4
andrejvb <andre.jvb@gmail.com>
2022-09-29 18:54:34 -0300

    conclui desafio 07

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,7 @@
 // Desafio 1
 function compareTrue(param1, param2) {
   if (param1 === true && param2 === true) {
-    return true;    
+    return true;
   } return false;
 }
 
@@ -15,9 +15,8 @@ function calcArea(param1, param2) {
 
 // Desafio 3
 function splitSentence(param1) {
- return param1.split(' ');
+  return param1.split(' ');
 }
-
 
 // splitSentence('Hj vai chover');
 // console.log(divisorPal);
@@ -30,7 +29,7 @@ function splitSentence(param1) {
 // }
 
 function concatName(param1) {
-  return param1[param1.length -1] + ', ' + param1[0]
+  return param1[param1.length - 1] + ', ' + param1[0]
 }
 
 // let nomes = ["Andre", "Rapha", "Ze"];
@@ -40,16 +39,11 @@ function concatName(param1) {
 
 // Desafio 5
 
-
-
 function footballPoints(wins, ties) {
   return (wins * 3) + (ties * 1);
 }
 
-
 // console.log(footballPoints(20, 20));
-
-
 
 
 // Desafio 6
@@ -59,7 +53,7 @@ function footballPoints(wins, ties) {
 //         if (param1 > param1[index]) {
 //       numeroMaior = param1[index]
 //     };
-   
+
 //   }
 // }
 
@@ -79,87 +73,65 @@ function highestCount(param1) {
     if (param1[index] === numeroMaior) {
       contador += 1;
     }
-    return contador;
   }
+  return contador;
+}
+
+
+  console.log(highestCount([9, 1, 9, 3, 9, 5, 7]));
+
+
+  // Desafio 7
+  function catAndMouse(mouse, cat1, cat2) {
+    
+    let distancecat1 = Math.abs (mouse - cat1);
+    let distancecat2 = Math.abs (mouse - cat2);
+    let result = '';
+
+        
+    if (distancecat1 > distancecat2) {
+      return result = 'cat1';      
+    } 
+    else if (distancecat2 > distancecat1) {
+      return result = 'cat2';    
+    } else {
+      return result = 'os gatos trombam e o rato foge';
+    }
   
-  
-
-
-
-console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
-
-
-// function highestCount(array) {
-//   let maior = Number.NEGATIVE_INFINITY;
-//   let contador = 0;
-//   for (let number of array){
-//     if (number >= maior){
-//       maior = number;
-//     }
-//   }
-//   for (let number of array){
-//     if (number === maior){
-//       contador += 1;
-//     }
-//   }
-//   return contador;
-// }
-
-function highestCount(array) {
-  let greater;
-  let count = 0;
-
-if (count == 0) {
-  for (let i = 0; i < array.length; i += 1){
-      if (array[i] > array[(i + 1)] ) {
-          greater = array[i];
-      }
   }
-  for (i = 0; i < array.length; i += 1) {
-      if (greater === array[i]) {
-          count += 1;
-      }
+
+// console.log(catAndMouse(10,5,15));
+
+
+
+  // Desafio 8
+  function fizzBuzz() {
+    // seu código aqui
   }
-}
 
-return count;
+  // Desafio 9
+  function encode() {
+    // seu código aqui
+  }
+  function decode() {
+    // seu código aqui
+  }
 
+  // Desafio 10
+  function techList() {
+    // seu código aqui
+  }
 
-console.log(highestCount([9, 1, 9, 3, 9, 5, 7]));
-
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
-
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
-
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
-
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
-
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
-  techList,
-};
+  module.exports = {
+    calcArea,
+    catAndMouse,
+    compareTrue,
+    concatName,
+    decode,
+    encode,
+    fizzBuzz,
+    footballPoints,
+    highestCount,
+    splitSentence,
+    techList,
+  };
